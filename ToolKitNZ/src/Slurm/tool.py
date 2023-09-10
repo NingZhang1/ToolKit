d7c7dee83585a09975c20f7671a71e48ee5d730f
NingZhang1 <ningzhang1024@gmail.com>
2023-09-10 15:10:55 -0700

    update

--- ToolKitNZ/src/Slurm/tool.py
+++ ToolKitNZ/src/Slurm/tool.py
@@ -12,7 +12,10 @@ def generate_slurm_input(
         nodes: int = 1,
         partition: str = "serial,parallel",
         # node_range: list = [0, 10],
+        # not_equipment = "pauling[001-040]",
+        not_equipment = None,
         memory: int = 100,
+        additional_option = None,
         commond: str = "python3 main.py",
 ):
     with open(filename, 'w') as f:
@@ -27,9 +30,13 @@ def generate_slurm_input(
         f.write("#SBATCH --nodes=%d\n" % (nodes))
         if exclusive:
             f.write("#SBATCH --exclusive\n")
-        f.write("#SBATCH -x pauling[001-040]\n")
+        if not_equipment is not None:
+            f.write("#SBATCH -x %s\n" % (not_equipment))
         f.write("#SBATCH --mem=%dG\n" % (memory))
         f.write("export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE\n")
+        if additional_option is not None:
+            for tmp in additional_option:
+                f.write("%s" % (tmp))
         f.write("%s\n" % (commond))
 
     # pass
